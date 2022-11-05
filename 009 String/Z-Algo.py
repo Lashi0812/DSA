c718a749701099cf2eec239e6c97ba832c10c79b
Lashi <lashi0812@gmail.com>
2022-11-05 21:09:25 +0530

    alter the z algo

--- 009 String/Z-Algo.py
+++ 009 String/Z-Algo.py
@@ -1,62 +1,58 @@
-# Given a string S , for every index i find the length of the longest substring from it which is equal to prefix of whole string
+# Given a string S , for every index i find the length of the longest substring from it which is equal to prefix of
+# whole string
 
-class Solution():
-    def bruteForce(self,s):
-        z_array = [len(s)]  
-        for i in range(1,len(s)):
-            j = i 
-            while j<len(s) and s[j]==s[j-i]:
-                j+=1
-            z_array.append(j-i)
+class Solution:
+    def bruteForce(self, s):
+        z_array = [len(s)]
+        for i in range(1, len(s)):
+            j = i
+            while j < len(s) and s[j] == s[j - i]:
+                j += 1
+            z_array.append(j - i)
         return z_array
 
-    def zAlgo(self,s):
-       
+    def zAlgo(self, s):
+
         rightBoxStart = 0
-        rightBoxEnd   = 0
+        rightBoxEnd = 0
         z_array = [len(s)]
 
-        for i in range(1,len(s)):   
-                        
+        for i in range(1, len(s)):
+
             # When we are out of the Box
-            if  i > rightBoxEnd: 
+            if i > rightBoxEnd:
                 rightBoxStart = i
-                rightBoxEnd   = i
+                rightBoxEnd = i
 
                 # print("Applying the brute for ",i)
-                
+
                 # brute force
-                while rightBoxEnd<len(s) and s[rightBoxEnd]==s[rightBoxEnd-rightBoxStart]:
-                    rightBoxEnd+=1             
-                
-                z_array.append(rightBoxEnd-rightBoxStart)
+                while rightBoxEnd < len(s) and s[rightBoxEnd] == s[rightBoxEnd - rightBoxStart]:
+                    rightBoxEnd += 1
+
+                z_array.append(rightBoxEnd - rightBoxStart)
                 # update the box
                 rightBoxEnd -= 1
 
             # when we are inside the box
             else:
                 j = i - rightBoxStart
-                if z_array[j] < rightBoxEnd-i+1:
+                if z_array[j] < rightBoxEnd - i + 1:
                     # print("Copying the element for ",i)
                     # just copy
                     z_array.append(z_array[j])
                 else:
                     rightBoxStart = i
-                    rightBoxEnd +=1
-                    while rightBoxEnd<len(s) and s[rightBoxEnd] == s[rightBoxEnd-rightBoxStart]:
-                        rightBoxEnd+=1 
-                      
-                    z_array.append(rightBoxEnd-rightBoxStart)
+                    rightBoxEnd += 1
+                    while rightBoxEnd < len(s) and s[rightBoxEnd] == s[rightBoxEnd - rightBoxStart]:
+                        rightBoxEnd += 1
+
+                    z_array.append(rightBoxEnd - rightBoxStart)
 
                     # update the box                    
                     rightBoxEnd -= 1
 
-
         return z_array
-
-            
-
-
 
 
 if __name__ == "__main__":
@@ -68,4 +64,3 @@ if __name__ == "__main__":
     print(test.bruteForce(s1))
     print(test.zAlgo(s1))
     print(test.zAlgo("slak@mynameislakshman"))
-        
